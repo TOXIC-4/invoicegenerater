6b020a8c4191f014fe19171d3d2a36e3cacc8826
Henil Khokhariya <khokhariyahenil@gmail.com>
2024-08-02 17:57:36 +0530

    a

--- src/Client/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loader({ loader }) {
-  return (
-    <div className={`loader-box ${loader ? "" : "invisible"}`}>
-      <img src={require("../Image/Logo/771.gif")} alt="Loading..." />
-    </div>
-  );
-}
--- src/MainApi.jsx
+++
@@ -1,5 +0,0 @@
-// const MainApi = "https://invoice-server-pi.vercel.app";
-const MainApi = "https://invoiceserver-nfyb.onrender.com";
-// const MainApi = "http://localhost:5000";
-
-export default MainApi;
